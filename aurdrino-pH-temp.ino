8f94b9b35fc5d06b3a8410601aea287fbc145abc
Michael Maiclein Howard <maiclein@simpledata.com>
2020-01-04 16:29:43 -0500

    Compiles clean.

--- aurdrino-pH-temp.ino
+++ aurdrino-pH-temp.ino
@@ -19,9 +19,15 @@
 
 #include "DFRobot_PH.h"
 #include <EEPROM.h>
+#include <OneWire.h>
 
 #define PH_PIN A1
-float voltage,phValue,temperature = 25;
+int TEMP_PIN = 2; //DS18S20 Signal pin on digital 2
+
+//Temperature chip i/o
+OneWire ds(TEMP_PIN);  // on digital pin 2
+
+float voltage,phValu;
 // Declare ph as a /DFRobot_PH object
 DFRobot_PH ph;
 
@@ -34,64 +40,67 @@ void setup()
 
 void loop()
 {
-    static unsigned long timepoint = millis();
-    if(millis()-timepoint>1000U)  //time interval: 1s
-    {
-      timepoint = millis();
-      voltage = analogRead(PH_PIN)/1024.0*5000;  // read the voltage
-      //temperature = readTemperature();  // read your temperature sensor to execute temperature compensation
-      phValue = ph.readPH(voltage,temperature);  // convert voltage to pH with temperature compensation
-      Serial.print("temperature:");
-      Serial.print(temperature,1);
-      Serial.print("^C  pH:");
-      Serial.println(phValue,2);
-    }
-    ph.calibration(voltage,temperature);  // calibration process by Serail CMD
+  float temperature;  
+  float phValue;
+  static unsigned long timepoint = millis();
+  
+  if(millis()-timepoint>1000U)  //time interval: 1s
+  {
+    timepoint = millis();
+    voltage = analogRead(PH_PIN)/1024.0*5000;  // read the voltage
+    temperature = readTemperature(); // read your temperature sensor to execute temperature compensation
+    phValue = ph.readPH(voltage,temperature);  // convert voltage to pH with temperature compensation
+    Serial.print("temperature:");
+    Serial.print(temperature,1);
+    Serial.print("^C  pH:");
+    Serial.println(phValue,2);
+  }
+  ph.calibration(voltage,temperature);  // calibration process by Serail CMD
 }
 
 float readTemperature()
 {
   //returns the temperature from one DS18S20 in DEG Celsius
 
-//  byte data[12];
-//  byte addr[8];
-//
-//  if ( !ds.search(addr)) {
-//      //no more sensors on chain, reset search
-//      ds.reset_search();
-//      return -1000;
-//  }
-//
-//  if ( OneWire::crc8( addr, 7) != addr[7]) {
-//      Serial.println("CRC is not valid!");
-//      return -1000;
-//  }
-//
-//  if ( addr[0] != 0x10 && addr[0] != 0x28) {
-//      Serial.print("Device is not recognized");
-//      return -1000;
-//  }
-//
-//  ds.reset();
-//  ds.select(addr);
-//  ds.write(0x44,1); // start conversion, with parasite power on at the end
-//
-//  byte present = ds.reset();
-//  ds.select(addr);
-//  ds.write(0xBE); // Read Scratchpad
-//
-//
-//  for (int i = 0; i < 9;  i++  ) { // we need 9 bytes
-//    data[i] = ds.read();
-//  }
-//
-//  ds.reset_search();
-//
-//  byte MSB = data[1];
-//  byte LSB = data[0];
-//
-//  float tempRead = ((MSB << 8) | LSB); //using two's compliment
-//  float TemperatureSum = tempRead / 16;
-//
-//  return TemperatureSum;
+  byte data[12];
+  byte addr[8];
+
+  if ( !ds.search(addr)) {
+      //no more sensors on chain, reset search
+      ds.reset_search();
+      return -1000;
+  }
+
+  if ( OneWire::crc8( addr, 7) != addr[7]) {
+      Serial.println("CRC is not valid!");
+      return -1000;
+  }
+
+  if ( addr[0] != 0x10 && addr[0] != 0x28) {
+      Serial.print("Device is not recognized");
+      return -1000;
+  }
+
+  ds.reset();
+  ds.select(addr);
+  ds.write(0x44,1); // start conversion, with parasite power on at the end
+
+  byte present = ds.reset();
+  ds.select(addr);
+  ds.write(0xBE); // Read Scratchpad
+
+
+  for (int i = 0; i < 9;  i++  ) { // we need 9 bytes
+    data[i] = ds.read();
+  }
+
+  ds.reset_search();
+
+  byte MSB = data[1];
+  byte LSB = data[0];
+
+  float tempRead = ((MSB << 8) | LSB); //using two's compliment
+  float TemperatureSum = tempRead / 16;
+
+  return TemperatureSum;
 }
